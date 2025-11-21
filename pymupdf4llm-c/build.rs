439f0c44db11ab493a326a91eb19f6a86be9a752
intercepted16 <162676516+intercepted16@users.noreply.github.com>
2025-11-21 00:00:14 +0000

    chore: include MUpdf in codebase for easier compilation

--- pymupdf4llm-c/build.rs
+++ pymupdf4llm-c/build.rs
@@ -5,10 +5,14 @@ use cmake::Config;
 fn main() {
     let crate_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     let c_src_dir = crate_dir.join("c_src");
+    let project_root = crate_dir.parent().unwrap();
+    let mupdf_lib_path = project_root.join("lib").join("mupdf");
 
     // Build using cmake crate
     let dst = Config::new(&c_src_dir)
         .profile("Release")
+        .define("MUPDF_LIB_DIR", mupdf_lib_path.to_str().unwrap())
+        .define("PROJECT_ROOT_DIR", project_root.to_str().unwrap())
         .build_target("tomd")
         .build();
 
