a6b8946e083672d3685a9aff9ecf49a31f6682d6
intercepted16 <162676516+intercepted16@users.noreply.github.com>
2025-12-13 16:23:24 +1100

    fix: more platform support

--- include/platform_compat.h
+++ include/platform_compat.h
@@ -1,84 +1,72 @@
-#ifndef PLATFORM_COMPAT_H
-#define PLATFORM_COMPAT_H
+#pragma once
 
-#ifndef _GNU_SOURCE
-#define _GNU_SOURCE
-#endif
-
-#ifndef _POSIX_C_SOURCE
-#define _POSIX_C_SOURCE 200809L
-#endif
-
+/* ===================== PLATFORM DETECTION ===================== */
 #ifdef _WIN32
-#include <direct.h>
-#define strncasecmp _strnicmp
-#define strcasecmp _stricmp
+#define PLATFORM_WINDOWS 1
+#else
+#define PLATFORM_WINDOWS 0
 #endif
 
 #ifdef __APPLE__
-#include <strings.h>   // for strncasecmp, strcasecmp
-#include <sys/types.h>
-#include <sys/sysctl.h>
+#define PLATFORM_MAC 1
+#include <TargetConditionals.h>
+#else
+#define PLATFORM_MAC 0
 #endif
 
-#include "mupdf/fitz.h"
-
-#ifndef FZ_STEXT_CLIP
-#define FZ_STEXT_CLIP 1
+#ifdef __linux__
+#define PLATFORM_LINUX 1
+#else
+#define PLATFORM_LINUX 0
 #endif
 
-#ifndef FZ_STEXT_ACCURATE_BBOXES
-#define FZ_STEXT_ACCURATE_BBOXES 2
-#endif
-
-#ifndef FZ_STEXT_COLLECT_STYLES
-#define FZ_STEXT_COLLECT_STYLES 32768
-#endif
-
-#ifndef FZ_STEXT_USE_GID_FOR_UNKNOWN_UNICODE
-#define FZ_STEXT_USE_GID_FOR_UNKNOWN_UNICODE 4
-#endif
-
-#ifndef FZ_STEXT_PRESERVE_LIGATURES
-#define FZ_STEXT_PRESERVE_LIGATURES 8
-#endif
-
-#ifndef FZ_STEXT_PRESERVE_WHITESPACE
-#define FZ_STEXT_PRESERVE_WHITESPACE 16
-#endif
-
-#ifndef FZ_FONT_FLAG_BOLD
-#define FZ_FONT_FLAG_BOLD 1
-#endif
-
-#ifndef FZ_FONT_FLAG_ITALIC
-#define FZ_FONT_FLAG_ITALIC 2
-#endif
-
+/* ===================== EXPORT MACRO ===================== */
+#ifndef EXPORT
 #ifdef _WIN32
 #define EXPORT __declspec(dllexport)
 #else
-#define EXPORT __attribute__((visibility("default")))
+#define EXPORT
+#endif
 #endif
 
-/* --- Added CPU detection helper for multiprocess --- */
-static inline int get_num_cores(void)
-{
-    int num_cores = 1;
+/* ===================== LEGACY TYPE FIXES FOR MAC ===================== */
+#if PLATFORM_MAC
+typedef unsigned int    u_int;
+typedef unsigned char   u_char;
+typedef unsigned short  u_short;
 
-#if defined(_SC_NPROCESSORS_ONLN)
-    long n = sysconf(_SC_NPROCESSORS_ONLN);
-    if (n > 0)
-        num_cores = (int)n;
-#elif defined(__APPLE__)
-    int n;
-    size_t len = sizeof(n);
-    int mib[2] = {CTL_HW, HW_AVAILCPU};
-    if (sysctl(mib, 2, &n, &len, NULL, 0) == 0 && n > 0)
-        num_cores = n;
-#endif
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/sysctl.h>
+#include <strings.h>
 
-    return num_cores;
+/* get number of CPU cores */
+static int get_num_cores() {
+    int nm[2];
+    size_t len = sizeof(int);
+    int count;
+    nm[0] = CTL_HW;
+    nm[1] = HW_NCPU;
+    sysctl(nm, 2, &count, &len, NULL, 0);
+    return count;
 }
+#endif
 
-#endif // PLATFORM_COMPAT_H
+/* ===================== LINUX CPU CORES ===================== */
+#if PLATFORM_LINUX
+#include <unistd.h>
+
+/* get number of CPU cores */
+static int get_num_cores() {
+    return sysconf(_SC_NPROCESSORS_ONLN);
+}
+#endif
+
+/* ===================== MATH/UTILS ===================== */
+#include <math.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifndef NAN
+#define NAN (__builtin_nanf("0x7fc00000"))
+#endif
